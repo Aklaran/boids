2e84181c408ec06a8f055d67297a65c8e056a094
Siri Tembunkiart <s.tembunkiart@gmail.com>
2015-03-25 15:02:17 -0400

    stuff that doesn't work

--- boids/boids/Scenes/BoidScene.m
+++ boids/boids/Scenes/BoidScene.m
@@ -8,13 +8,45 @@
 
 #import "BoidScene.h"
 
+@interface BoidScene ()
+
+@property BOOL contentCreated;
+
+@end
+
 @implementation BoidScene
 
 - (void)viewWillAppear:(BOOL)animated
 {
-    BoidScene* boid = [[BoidScene alloc] initWithSize:CGSizeMake(768,1024)];
+    BoidScene* boid = [[BoidScene alloc] initWithSize:CGSizeMake(1024,768)];
     SKView *spriteView = (SKView *) self.view;
     [spriteView presentScene: boid];
 }
+
+- (void)didMoveToView: (SKView *) view
+{
+    if (!self.contentCreated)
+    {
+        [self createSceneContents];
+        self.contentCreated = YES;
+    }
+}
+
+- (void)createSceneContents
+{
+    self.backgroundColor = [SKColor blueColor];
+    self.scaleMode = SKSceneScaleModeAspectFit;
+    [self addChild: [self newHelloNode]];
+}
+
+- (SKLabelNode *)newHelloNode
+{
+    SKLabelNode *helloNode = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+    helloNode.text = @"Hello, World!";
+    helloNode.fontSize = 42;
+    helloNode.position = CGPointMake(CGRectGetMidX(self.frame),CGRectGetMidY(self.frame));
+    return helloNode;
+}
+
 
 @end
