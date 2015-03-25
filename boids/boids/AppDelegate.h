85bdffdd7a16dce78dc0abebf26dd9236b9c3525
Siri Tembunkiart <s.tembunkiart@gmail.com>
2015-03-25 13:54:13 -0400

    the project

---
+++ boids/boids/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  boids
+//
+//  Created by Siri Tembunkiart on 3/25/15.
+//  Copyright (c) 2015 Bo Tembunkiart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ boids/boids/Controllers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  boids
+//
+//  Created by Siri Tembunkiart on 3/25/15.
+//  Copyright (c) 2015 Bo Tembunkiart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
